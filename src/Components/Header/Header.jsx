cb10598d3c877b27a2604a9dd07905e6c5687c27
prasanth <kprasanth213@gmail.com>
2022-10-25 17:53:30 +0530

    uploading 1st hosting

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
+import ME from '../../assets/first.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -15,7 +15,7 @@ const Header = () => {
             <HeaderSocials/>
 
             <div className="me">
-                <img src={ME} alt="" />
+                <img src={ME} alt=""/>
             </div>
 
             <a href="#contact" className='scroll_down'> Scroll Down</a>
